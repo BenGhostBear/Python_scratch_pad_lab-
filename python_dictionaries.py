4e8cd18bf808de8c102d4dd59710f3ebd64813dc
Damian <147666866+BenGhostBear@users.noreply.github.com>
2024-03-11 14:05:39 -0700

    Create python_dictionaries.py

---
+++ python_dictionaries.py
@@ -0,0 +1,12 @@
+my_dict = {'apple': 1, 'banana' : 2, 'cherry' : 3}
+
+print(my_dict['apple'])
+
+my_dict['date'] = 4
+
+my_dict['apple'] = 5
+
+del my_dict['banana']
+
+for key, values in my_dict.items():
+print(f'key: {key}, Value: {value}')
